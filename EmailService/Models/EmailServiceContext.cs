4c00c126380a2914f22845c3819721bf61cfcb2e
Romit Sagu <romit.sagu@hotmail.com>
2025-12-01 21:43:31 -0500

    Switch to PostgresSQL

--- EmailService/Models/EmailServiceContext.cs
+++ EmailService/Models/EmailServiceContext.cs
@@ -27,123 +27,113 @@ public partial class EmailServiceContext : DbContext
     public virtual DbSet<VwMessageQueue> VwMessageQueues { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["EmailServiceEntities"].ConnectionString);
+        => optionsBuilder.UseNpgsql(ConfigurationManager.ConnectionStrings["EmailServiceEntities"].ConnectionString);
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.HasDefaultSchema("dbo");
+
         modelBuilder.Entity<Credential>(entity =>
         {
+            entity.ToTable("credentials");
+
             entity.HasKey(e => e.Uid).HasName("PK__Credenti__C5B19602CC1516BE");
 
-            entity.Property(e => e.Uid).HasColumnName("UID");
-            entity.Property(e => e.Password).IsUnicode(false);
-            entity.Property(e => e.UserName)
-                .HasMaxLength(250)
-                .IsUnicode(false);
+            entity.Property(e => e.Uid).HasColumnName("uid");
+            entity.Property(e => e.UserName).HasColumnName("username");
+            entity.Property(e => e.Password).HasColumnName("password");
         });
 
         modelBuilder.Entity<IncomingMessageRecepient>(entity =>
         {
+            entity.ToTable("incomingmessagerecepients");
+
             entity.HasKey(e => e.Uid).HasName("PK__Incoming__C5B196028AB4035E");
 
-            entity.Property(e => e.Uid).HasColumnName("UID");
-            entity.Property(e => e.EmailSentDateTime).HasColumnType("datetime");
-            entity.Property(e => e.IncomingMessageId).HasColumnName("IncomingMessageID");
-            entity.Property(e => e.IsBcc).HasColumnName("isBCC");
-            entity.Property(e => e.IsCc).HasColumnName("isCC");
-            entity.Property(e => e.NotifiedDateTime).HasColumnType("datetime");
-            entity.Property(e => e.ReadDateTime).HasColumnType("datetime");
-            entity.Property(e => e.RecepientEmail)
-                .HasMaxLength(100)
-                .IsUnicode(false);
+            entity.Property(e => e.Uid).HasColumnName("uid");
+            entity.Property(e => e.IncomingMessageId).HasColumnName("incomingmessageid");
+            entity.Property(e => e.RecepientEmail).HasColumnName("recepientemail");
+            entity.Property(e => e.IsCc).HasColumnName("iscc");
+            entity.Property(e => e.IsBcc).HasColumnName("isbcc");
+            entity.Property(e => e.IsNotified).HasColumnName("isnotified");
+            entity.Property(e => e.NotifiedDateTime).HasColumnName("notifieddatetime");
+            entity.Property(e => e.IsRead).HasColumnName("isread");
+            entity.Property(e => e.ReadDateTime).HasColumnName("readdatetime");
+            entity.Property(e => e.IsProcessed).HasColumnName("isprocessed");
+            entity.Property(e => e.IsEmailSent).HasColumnName("isemailsent");
+            entity.Property(e => e.EmailSentDateTime).HasColumnName("emailsentdatetime");
         });
 
         modelBuilder.Entity<IncomingMessageText>(entity =>
         {
             entity.HasKey(e => e.Uid).HasName("PK__Incoming__C5B1960204776D18");
 
-            entity.ToTable("IncomingMessageText");
+            entity.ToTable("incomingmessagetext");
 
-            entity.Property(e => e.Uid).HasColumnName("UID");
-            entity.Property(e => e.CreatedEmail)
-                .HasMaxLength(100)
-                .IsUnicode(false);
-            entity.Property(e => e.CreatedName)
-                .HasMaxLength(100)
-                .IsUnicode(false);
-            entity.Property(e => e.Creation).HasColumnType("datetime");
-            entity.Property(e => e.MessageType)
-                .HasMaxLength(50)
-                .IsUnicode(false);
-            entity.Property(e => e.Parameters)
-                .HasMaxLength(5000)
-                .IsUnicode(false);
-            entity.Property(e => e.Title)
-                .HasMaxLength(250)
-                .IsUnicode(false);
+            entity.Property(e => e.Uid).HasColumnName("uid");
+            entity.Property(e => e.Title).HasColumnName("title");
+            entity.Property(e => e.BodyHtml).HasColumnName("bodyhtml");
+            entity.Property(e => e.Creation).HasColumnName("creation");
+            entity.Property(e => e.CreatedEmail).HasColumnName("createdemail");
+            entity.Property(e => e.CreatedName).HasColumnName("createdname");
+            entity.Property(e => e.IsEmail).HasColumnName("isemail");
+            entity.Property(e => e.MessageType).HasColumnName("messagetype");
+            entity.Property(e => e.Parameters).HasColumnName("parameters");
+            entity.Property(e => e.IsSecure).HasColumnName("issecure");
+            entity.Property(e => e.IsImportantTag).HasColumnName("isimportanttag");
         });
 
         modelBuilder.Entity<VwIncomingMessage>(entity =>
         {
             entity
                 .HasNoKey()
-                .ToView("vw_IncomingMessages");
+                .ToView("vw_incomingmessages");
 
-            entity.Property(e => e.CreatedDate)
-                .HasMaxLength(30)
-                .IsUnicode(false);
-            entity.Property(e => e.CreatedEmail)
-                .HasMaxLength(100)
-                .IsUnicode(false);
-            entity.Property(e => e.CreatedName)
-                .HasMaxLength(100)
-                .IsUnicode(false);
-            entity.Property(e => e.Creation).HasColumnType("datetime");
-            entity.Property(e => e.IncomingMessageId).HasColumnName("IncomingMessageID");
-            entity.Property(e => e.IsBcc).HasColumnName("isBCC");
-            entity.Property(e => e.IsCc).HasColumnName("isCC");
-            entity.Property(e => e.MessageType)
-                .HasMaxLength(50)
-                .IsUnicode(false);
-            entity.Property(e => e.NotifiedDateTime).HasColumnType("datetime");
-            entity.Property(e => e.ReadDateTime).HasColumnType("datetime");
-            entity.Property(e => e.RecepientEmail)
-                .HasMaxLength(100)
-                .IsUnicode(false);
-            entity.Property(e => e.Title)
-                .HasMaxLength(250)
-                .IsUnicode(false);
-            entity.Property(e => e.Uid).HasColumnName("UID");
+            entity.Property(e => e.Uid).HasColumnName("uid");
+            entity.Property(e => e.IncomingMessageId).HasColumnName("incomingmessageid");
+            entity.Property(e => e.RecepientEmail).HasColumnName("recepientemail");
+            entity.Property(e => e.IsCc).HasColumnName("iscc");
+            entity.Property(e => e.IsBcc).HasColumnName("isbcc");
+            entity.Property(e => e.Title).HasColumnName("title");
+            entity.Property(e => e.Creation).HasColumnName("creation");
+            entity.Property(e => e.CreatedDate).HasColumnName("createddate");
+            entity.Property(e => e.CreatedEmail).HasColumnName("createdemail");
+            entity.Property(e => e.CreatedName).HasColumnName("createdname");
+            entity.Property(e => e.IsProcessed).HasColumnName("isprocessed");
+            entity.Property(e => e.IsEmailSent).HasColumnName("isemailsent");
+            entity.Property(e => e.IsSecure).HasColumnName("issecure");
+            entity.Property(e => e.ReadDateTime).HasColumnName("readdatetime");
+            entity.Property(e => e.NotifiedDateTime).HasColumnName("notifieddatetime");
+            entity.Property(e => e.BodyHtml).HasColumnName("bodyhtml");
+            entity.Property(e => e.IsEmail).HasColumnName("isemail");
+            entity.Property(e => e.MessageType).HasColumnName("messagetype");
+            entity.Property(e => e.IsRead).HasColumnName("isread");
+            entity.Property(e => e.IsImportantTag).HasColumnName("isimportanttag");
         });
 
         modelBuilder.Entity<VwMessageQueue>(entity =>
         {
             entity
                 .HasNoKey()
-                .ToView("vw_MessageQueue");
+                .ToView("vw_messagequeue");
 
-            entity.Property(e => e.CreatedEmail)
-                .HasMaxLength(100)
-                .IsUnicode(false);
-            entity.Property(e => e.CreatedName)
-                .HasMaxLength(100)
-                .IsUnicode(false);
-            entity.Property(e => e.Creation).HasColumnType("datetime");
-            entity.Property(e => e.IncomingMessageId).HasColumnName("IncomingMessageID");
-            entity.Property(e => e.IsBcc).HasColumnName("isBCC");
-            entity.Property(e => e.IsCc).HasColumnName("isCC");
-            entity.Property(e => e.NotifiedDateTime).HasColumnType("datetime");
-            entity.Property(e => e.Priority)
-                .HasMaxLength(4)
-                .IsUnicode(false);
-            entity.Property(e => e.ReadDateTime).HasColumnType("datetime");
-            entity.Property(e => e.RecepientEmail)
-                .HasMaxLength(100)
-                .IsUnicode(false);
-            entity.Property(e => e.Title)
-                .HasMaxLength(250)
-                .IsUnicode(false);
-            entity.Property(e => e.Uid).HasColumnName("UID");
+            entity.Property(e => e.Uid).HasColumnName("uid");
+            entity.Property(e => e.IncomingMessageId).HasColumnName("incomingmessageid");
+            entity.Property(e => e.RecepientEmail).HasColumnName("recepientemail");
+            entity.Property(e => e.Title).HasColumnName("title");
+            entity.Property(e => e.IsRead).HasColumnName("isread");
+            entity.Property(e => e.Creation).HasColumnName("creation");
+            entity.Property(e => e.CreatedEmail).HasColumnName("createdemail");
+            entity.Property(e => e.CreatedName).HasColumnName("createdname");
+            entity.Property(e => e.IsSecure).HasColumnName("issecure");
+            entity.Property(e => e.NotifiedDateTime).HasColumnName("notifieddatetime");
+            entity.Property(e => e.ReadDateTime).HasColumnName("readdatetime");
+            entity.Property(e => e.Priority).HasColumnName("priority");
+            entity.Property(e => e.BodyHtml).HasColumnName("bodyhtml");
+            entity.Property(e => e.IsEmail).HasColumnName("isemail");
+            entity.Property(e => e.IsCc).HasColumnName("iscc");
+            entity.Property(e => e.IsBcc).HasColumnName("isbcc");
+            entity.Property(e => e.IsImportantTag).HasColumnName("isimportanttag");
         });
 
         OnModelCreatingPartial(modelBuilder);
